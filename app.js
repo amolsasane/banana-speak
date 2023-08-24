87a1bab09b32463f4aa2d937b94a50c70f942a6f
Amol Sasane <amolsasane1429@gmail.com>
2023-08-24 18:54:49 +0530

    server import

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 btnTranslate.addEventListener("click", clickHandler)
 
-function clickHandler() {
-     console.log("Clicked!");
-     output.innerText = txtInput.value;
+function updateURL(input) {
+     return serverURL + "?" + "text=" + input 
 }
+
+function clickHandler() {
+     var inputText = txtInput.value;
+     fetch(updateURL(inputText))
+     .then(response => response.json())
+     .then(json => {
+         var translatedText = json.contents.translated;
+         output.innerText = translatedText;
+     })
+}    
